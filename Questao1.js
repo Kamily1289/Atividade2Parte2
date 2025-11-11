38a810a1f449edb83216d6226e7c11927f688bd1
Kamily1289 <kamilyguedesmariakamilyguedes@gmail.com>
2025-11-11 14:07:07 -0300

    Add user object with JSON stringify/parse example
    Create a user object and demonstrate JSON conversion.

---
+++ Questao1.js
@@ -0,0 +1,12 @@
+const user = {
+nome: "Ana Silva",
+idade: 30,
+habilidades: ["JavaScript", "React", "Node.js"],
+ativo: true
+};
+// Converte o objeto Java Script para a String JSON
+const JsonString = JSON.stringify(user);
+console.log(JsonString);
+// recuperar para o objeto original 
+const ObjetoRecuperado = JSON.parse(JsonString);
+console.log(ObjetoRecuperado); // saída
