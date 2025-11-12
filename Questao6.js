88d5aef06c2545158f5358a60c1cab68873145f3
Kamily1289 <kamilyguedesmariakamilyguedes@gmail.com>
2025-11-12 12:41:48 -0300

    Implement parseJSONSafe function
    Add a safe JSON parsing function with error handling

--- Questao6.js
+++ Questao6.js
@@ -1,1 +1,13 @@
+function parseJSONSafe(jsonString) {
+  try {
+    const data = JSON.parse(jsonString);
+    return data;
+  } catch (error) {
+    console.error("Erro no JSON:", error.message);
+    return null;
+  }
+}
+
+const jsonInvalidoString = '{ "nome": "João", "idade": 36 }'; 
+console.log(parseJSONSafe(jsonInvalidoString));
 
